b7d084ff81cf122b3c8e5bd126e6ad57645062ab
Siak Hooi <73884114+siakhooi@users.noreply.github.com>
2022-12-31 10:19:04 +0000

    refactor Array tests into Array.test

---
+++ src/test/Array.test.ts
@@ -0,0 +1,17 @@
+import {Console} from '../main/Console';
+import {ALL_FUNCTIONS, ARRAY_TEST_DATA, TestDataUnit} from './TestData';
+
+describe.each(ALL_FUNCTIONS)('Array', (drawBoxFunction: Function) => {
+  test.each(ARRAY_TEST_DATA)(
+    `${drawBoxFunction.name} [$title]`,
+    (testData: TestDataUnit) => {
+      const allData: string[] = [];
+      Console.println = jest
+        .fn()
+        .mockImplementation(text => allData.push(text));
+
+      drawBoxFunction(testData.data);
+      expect(allData).toMatchSnapshot();
+    }
+  );
+});
--- src/test/TestData.ts
+++ src/test/TestData.ts
@@ -1,21 +1,49 @@
 import {
-  drawBoxThin,
-  drawBoxThick,
   drawBoxDouble,
-  drawBoxThinCurve,
-  drawBoxHorizontalThinVerticalThick,
+  drawBoxHorizontalDoubleVerticalThin,
   drawBoxHorizontalThickVerticalThin,
   drawBoxHorizontalThinVerticalDouble,
-  drawBoxHorizontalDoubleVerticalThin,
+  drawBoxHorizontalThinVerticalThick,
   drawBoxInnerDoubleOuterThin,
+  drawBoxInnerDoubleOuterThinCurve,
+  drawBoxInnerThickOuterThin,
+  drawBoxInnerThickOuterThinCurve,
   drawBoxInnerThinOuterDouble,
   drawBoxInnerThinOuterThick,
-  drawBoxInnerThickOuterThin,
-  drawBoxInnerDoubleOuterThinCurve,
-  drawBoxInnerThickOuterThinCurve,
+  drawBoxThick,
+  drawBoxThin,
+  drawBoxThinCurve,
 } from '../main/index';
 
 export const SIMPLE_TEST_DATA = ['A', '', 'Hello World', null];
+
+export type TestDataUnit = {
+  title: string;
+  data: string[][];
+};
+
+export const ARRAY_TEST_DATA: TestDataUnit[] = [
+  {title: 'Simple 1X1', data: [['In 1x1 array']]},
+  {
+    title: 'Simple 2x2',
+    data: [
+      ['Cell 1-1', 'Cell 2-1'],
+      ['Cell 1-2', 'Cell 2-2'],
+    ],
+  },
+  {
+    title: 'Variable Row Lengths 1+2',
+    data: [['Cell 1-1'], ['Cell 1-2', 'Cell 2-2']],
+  },
+  {
+    title: 'Variable Row Lengths 1+1+3',
+    data: [
+      ['', 'Cell 2-1 ABC'],
+      ['Cell 1-2 Avatar'],
+      ['Cell 1-3 Lemmings', 'Cell 2-3 A', 'Cell 3-3 Bata'],
+    ],
+  },
+];
 export const ALL_FUNCTIONS = [
   drawBoxThin,
   drawBoxThick,
