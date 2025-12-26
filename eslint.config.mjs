1847bdaa871a92709dcdf50b16dab7b4ecb7b25f
Siak Hooi <siakhooi@gmail.com>
2025-12-26 00:38:49 +0000

    upgrade gts to 7.0.0

---
+++ eslint.config.mjs
@@ -0,0 +1,93 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
+import pluginN from 'eslint-plugin-n';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['**/dist/', '**/coverage/', '**/node_modules/'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    plugins: {
+      prettier: pluginPrettier,
+      n: pluginN,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'block-scoped-var': 'error',
+      'eqeqeq': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'eol-last': 'error',
+      'prefer-arrow-callback': 'error',
+      'no-trailing-spaces': 'error',
+      'quotes': ['warn', 'single', {avoidEscape: true}],
+      'no-restricted-properties': [
+        'error',
+        {
+          object: 'describe',
+          property: 'only',
+        },
+        {
+          object: 'it',
+          property: 'only',
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/.prettierrc.js', '**/jest.config.js', '**/eslint.config.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: globals.node,
+    },
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+    rules: {
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-use-before-define': 'off',
+      '@typescript-eslint/no-warning-comments': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/ban-types': 'off',
+      '@typescript-eslint/camelcase': 'off',
+      'n/no-missing-import': 'off',
+      'n/no-empty-function': 'off',
+      'n/no-unsupported-features/es-syntax': 'off',
+      'n/no-missing-require': 'off',
+      'n/shebang': 'off',
+      'no-dupe-class-members': 'off',
+      'require-atomic-updates': 'off',
+    },
+  },
+  {
+    files: ['**/*.test.ts', '**/*.spec.ts'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unsafe-function-type': 'off',
+    },
+  },
+];
